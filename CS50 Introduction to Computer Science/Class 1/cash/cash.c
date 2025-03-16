7be0adfb0b653bfd769ad4be268a93afdad5fb88
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-03-16 23:39:22 +0100

    implemented the cash problem set

--- CS50 Introduction to Computer Science/Class 1/cash/cash.c
+++ CS50 Introduction to Computer Science/Class 1/cash/cash.c
@@ -5,19 +5,42 @@
 int main() 
 {
   int cents;
+
+  printf("Change owed: ");
+
   while (scanf("%d", &cents) != 1 || cents < 0) 
   {
       printf("Invalid input. Please enter a non-negative integer: ");
-      // Clear the input buffer
       while (getchar() != '\n');
   }
 
-  int quarter;
-  int nickel;
-  int dime;
+  int quarters = 0;
+  int dimes = 0;
+  int nickels = 0;
+  int pennies = 0;
   int total;
-  while(cents > 25) {
-    cents = cents - 25;
-    quarter = quarter + 1;
+  
+  while(cents >= 25) {
+    cents-=25;
+    quarters++;
+
   }
+  
+  while(cents >= 10) {
+    cents-=10;
+    dimes++;
+  } 
+  
+  while(cents >= 5) {
+    cents-=5;
+    nickels++;
+  }
+  
+  while(cents >= 1  ) {
+    cents-=1;
+    pennies++;
+  }
+
+  total  = quarters + dimes + nickels + pennies;
+  printf("Total: %d\n", total);
 }
