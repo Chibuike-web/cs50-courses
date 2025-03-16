6569d2c8d1a81973b72c9b823b5a01865273e3f4
Maduabuchi Chibuike <juniortechbro@gmail.com>
2025-03-16 23:44:30 +0100

    fixed spacing

--- CS50 Introduction to Computer Science/Class 1/mario-less/mario.c
+++ CS50 Introduction to Computer Science/Class 1/mario-less/mario.c
@@ -2,33 +2,33 @@
 
 int main(void)
 {
-    int height;
+  int height;
 
-    do
-    {
-        printf("Height: ");
+  do
+  {
+    printf("Height: ");
         
-        if (scanf("%d", &height) != 1)
-        {
-            height = -1;
-            scanf("%*s");
-        }
-    } while (height < 1 || height > 50); // Keep asking until valid
-
-    // Print the pyramid
-    for (int i = 0; i < height; i++)
+    if (scanf("%d", &height) != 1)
     {
-        for (int j = 0; j < height - i - 1; j++)
-        {
-            printf(" ");
-        }
-        for (int j = 0; j <= i; j++)
-        {
-            printf("#");
-        }
-        
-        printf("\n");
+      height = -1;
+      scanf("%*s");
     }
+  } while (height < 1 || height > 50); // Keep asking until valid
 
-    return 0;
+  // Print the pyramid
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = 0; j < height - i - 1; j++)
+    {
+      printf(" ");
+    }
+    for (int j = 0; j <= i; j++)
+    {
+      printf("#");
+    }
+    
+    printf("\n");
+  }
+
+  return 0;
 }
--- CS50 Introduction to Computer Science/Class 1/mario-more/mario.c
+++ CS50 Introduction to Computer Science/Class 1/mario-more/mario.c
@@ -2,49 +2,45 @@
 
 int main(void)
 {
-    int height;
+  int height;
 
-    do
+  do
+  {
+    printf("Height: ");
+    
+    if (scanf("%d", &height) != 1)
     {
-        printf("Height: ");
-        
-        if (scanf("%d", &height) != 1)
-        {
-            height = -1;
-            scanf("%*s");
-        }
-    } while (height < 1 || height > 50); // Keep asking until valid
+      height = -1;
+      scanf("%*s");
+    }
+  } while (height < 1 || height > 50); // Keep asking until valid
 
-    for (int i = 0; i < height; i++)
+  for (int i = 0; i < height; i++)
+  {
+    // Print spaces for left pyramid alignment
+    for (int j = 0; j < height - i - 1; j++)
     {
-      // Print spaces for left pyramid alignment
-        for (int j = 0; j < height - i - 1; j++)
-        {
-            printf(" ");
-        }
-
-      // Print left pyramid
-        for (int j = 0; j <= i; j++)
-        {
-            printf("#");
-            
-        }
-        
-        // Print gap between pyramids
-        printf("  ");
-
-        // Print right pyramid
-        for (int j = 0; j <= i; j++)
-        {
-            printf("#");
-            
-        }
-
-        // Move to the next line
-        printf("\n");
+      printf(" ");
     }
 
+    // Print left pyramid
+    for (int j = 0; j <= i; j++)
+    {
+      printf("#");
+    }
     
+    // Print gap between pyramids
+    printf("  ");
 
-    return 0;
+    // Print right pyramid
+    for (int j = 0; j <= i; j++)
+    {
+      printf("#");
+    }
+
+    // Move to the next line
+    printf("\n");
+  }
+
+  return 0;
 }
